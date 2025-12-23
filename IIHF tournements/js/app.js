4c3b32d44119d0d0c04ee4d6571317d8d67f489b
WatsonTheGreat <wbarberhyde@icloud.com>
2025-12-23 09:22:22 -0500

    Delete IIHF tournements directory

--- IIHF tournements/js/app.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById("menu");
-
-    if (menu.style.right === "0px") {
-        menu.style.right = "-200px";
-    } else {
-        menu.style.right = "0px";
-    }
-}
